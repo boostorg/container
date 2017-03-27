a2f3eddb053dbee0f2b84732f95208a9f1ca8882
tobias-loew <tobi@die-loews.de>
2017-03-27 12:14:15 +0200

    added alignment specification for small_vector
    added alignment specification for small_vector which ensures alignment
    of internal-storage. Default value of 0 retains original alignment

--- include/boost/container/container_fwd.hpp
+++ include/boost/container/container_fwd.hpp
@@ -115,7 +115,8 @@ template <class T, std::size_t Capacity>
 class static_vector;
 
 template < class T, std::size_t N
-         , class Allocator= new_allocator<T> >
+         , class Allocator= new_allocator<T>
+         , std::size_t alignment= 0 >
 class small_vector;
 
 template <class T
