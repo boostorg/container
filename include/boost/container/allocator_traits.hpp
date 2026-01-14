f818ee9bfd66c3c5c355bb7e3cc3394be7f8618c
Ion Gaztañaga <igaztanaga@gmail.com>
2026-01-14 12:36:54 +0100

    Delete alignment check, ubsan will be used to detect this in tests

--- include/boost/container/allocator_traits.hpp
+++ include/boost/container/allocator_traits.hpp
@@ -490,7 +490,6 @@ struct allocator_traits
       template <class T, class ...Args>
       inline static void construct(Allocator & a, T* p, BOOST_FWD_REF(Args)... args)
       {
-         BOOST_ASSERT(std::size_t(p) % dtl::alignment_of<T>::value == 0);
          BOOST_STATIC_CONSTEXPR bool value = ::boost::move_detail::and_
             < dtl::is_not_std_allocator<Allocator>
             , boost::container::dtl::has_member_function_callable_with_construct
