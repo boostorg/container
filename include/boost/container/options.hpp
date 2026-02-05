16202879653caabb8c6d49e6a817cabd8015ecd8
Ion Gaztañaga <igaztanaga@gmail.com>
2026-02-06 00:00:47 +0100

    Clarify the block_size/block_bytes option

--- include/boost/container/options.hpp
+++ include/boost/container/options.hpp
@@ -795,7 +795,7 @@ using string_options_t = typename boost::container::string_options<Options...>::
 #endif
 
 //!This option specifies the maximum size of a block in bytes: this delimites the number of contiguous elements
-//!that will be allocated by some containers as min(1u, BlockBytes/sizeof(value_type))
+//!that will be allocated by some segmented containers as min(1u, BlockBytes/sizeof(value_type))
 //!A value zero represents the default value.
 //!
 //!\tparam BlockBytes An unsigned integer value.
@@ -806,8 +806,8 @@ BOOST_INTRUSIVE_OPTION_CONSTANT(block_bytes, std::size_t, BlockBytes, block_byte
 //!\tparam SegmentBytes An unsigned integer value.
 BOOST_INTRUSIVE_OPTION_CONSTANT(segment_bytes, std::size_t, SegmentBytes, block_bytes)
 
-//!This option specifies the size of a block, delimites the number of contiguous elements
-//!that will be allocated by some containersas BlockSize.
+//!This option specifies the size of a block, delimites the number of contiguous elements (BlockSize)
+//!that will be allocated by segmented containers.
 //!For some containers (like deque/segmented_vector), a power of two value can improve performance.
 //!A value zero represents the default value.
 //!
