000372ebabebe893ead955e3722a359648e24745
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-17 15:27:30 +0100

    Avoid reference parameters in partition_scan

--- include/boost/container/experimental/segmented_partition.hpp
+++ include/boost/container/experimental/segmented_partition.hpp
@@ -33,7 +33,7 @@ FwdIt segmented_partition(FwdIt first, Sent last, Pred pred);
 namespace detail_algo {
 
 template <class FwdIt, class OutIter, class Pred>
-void partition_scan(FwdIt first, FwdIt last, OutIter& result, Pred& pred, non_segmented_iterator_tag)
+OutIter partition_scan(FwdIt first, FwdIt last, OutIter result, Pred pred, non_segmented_iterator_tag)
 {
    for(; first != last; ++first) {
       if(pred(*first)) {
@@ -41,38 +41,39 @@ void partition_scan(FwdIt first, FwdIt last, OutIter& result, Pred& pred, non_se
          ++result;
       }
    }
+   return result;
 }
 
 template <class SegIt, class OutIter, class Pred>
-void partition_scan(SegIt first, SegIt last, OutIter& result, Pred& pred, segmented_iterator_tag)
+OutIter partition_scan(SegIt first, SegIt last, OutIter result, Pred pred, segmented_iterator_tag)
 {
-   typedef segmented_iterator_traits<SegIt> traits;
-   typedef typename traits::local_iterator local_iterator;
+   typedef segmented_iterator_traits<SegIt>  traits;
+   typedef typename traits::local_iterator   local_iterator;
+   typedef typename traits::segment_iterator segment_iterator;
+
    typedef typename segmented_iterator_traits<local_iterator>::is_segmented_iterator is_local_seg_t;
-   typename traits::segment_iterator scur = traits::segment(first);
-   typename traits::segment_iterator slast = traits::segment(last);
-   local_iterator lcur = traits::local(first);
+   segment_iterator scur  = traits::segment(first);
+   segment_iterator slast = traits::segment(last);
+   local_iterator   lcur  = traits::local(first);
+
    if(scur == slast) {
-      partition_scan(lcur, traits::local(last), result, pred, is_local_seg_t());
+      return partition_scan(lcur, traits::local(last), result, pred, is_local_seg_t());
    }
    else {
-      partition_scan(lcur, traits::end(scur), result, pred,
-         is_local_seg_t());
+      result = partition_scan(lcur, traits::end(scur), result, pred, is_local_seg_t());
+
       for(++scur; scur != slast; ++scur)
-         partition_scan(traits::begin(scur), traits::end(scur), result, pred,
-            is_local_seg_t());
-      partition_scan(traits::begin(scur), traits::local(last), result, pred,
-         is_local_seg_t());
+         result = partition_scan(traits::begin(scur), traits::end(scur), result, pred, is_local_seg_t());
+
+      return partition_scan(traits::begin(scur), traits::local(last), result, pred, is_local_seg_t());
    }
 }
 
 template <class SegIter, class Pred>
-SegIter segmented_partition_dispatch
+BOOST_CONTAINER_FORCEINLINE SegIter segmented_partition_dispatch
    (SegIter first, SegIter last, Pred pred, segmented_iterator_tag)
 {
-   SegIter result = first;
-   partition_scan(first, last, result, pred, segmented_iterator_tag());
-   return result;
+   return partition_scan(first, last, first, pred, segmented_iterator_tag());
 }
 
 template <class FwdIt, class Sent, class Pred, class Tag>
