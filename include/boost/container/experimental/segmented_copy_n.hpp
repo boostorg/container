bb10a9e22a8cb91505b1770e5db6ef6b0090c4b0
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-17 10:46:46 +0100

    Erase dead code and add BOOST_CONTAINER_FORCEINLINE

--- include/boost/container/experimental/segmented_copy_n.hpp
+++ include/boost/container/experimental/segmented_copy_n.hpp
@@ -126,59 +126,14 @@ OutIter segmented_copy_n_dispatch
 //! Copies \c count elements from the range beginning at \c first to
 //! the range beginning at \c result. Exploits segmentation on input.
 template <class InIter, class Size, class OutIter>
-inline OutIter segmented_copy_n(InIter first, Size count, OutIter result)
+BOOST_CONTAINER_FORCEINLINE OutIter segmented_copy_n(InIter first, Size count, OutIter result)
 {
    typedef segmented_iterator_traits<InIter> traits;
    return detail_algo::segmented_copy_n_dispatch(first, count, result,
       typename traits::is_segmented_iterator());
 }
 
-/*
 
-
-template <class InIter, class Size, class OutIter>
-OutIter segmented_copy_n(InIter first, Size count, OutIter result);
-
-namespace detail_algo {
-
-template <class InIter, class Size, class OutIter>
-OutIter segmented_copy_n_dispatch
-   (InIter first, Size &count, OutIter result, non_segmented_iterator_tag);
-
-template <class InIter, class Size, class OutIter>
-OutIter copy_n_scan(InIter first, InIter last, Size& count, OutIter result, non_segmented_iterator_tag)
-{
-   Size local_count = count;  //Avoid aliasing the count parameter
-   for(; first != last && local_count > 0; ++first, ++result, --local_count)
-      *result = *first;
-   count = local_count;  //Restore the count parameter
-   return result;
-}
-
-template <class SegIt, class Size, class OutIter>
-OutIter copy_n_scan(SegIt first, SegIt last, Size& count, OutIter result, segmented_iterator_tag)
-{
-   typedef segmented_iterator_traits<SegIt> traits;
-   typedef typename traits::local_iterator local_iterator;
-   typedef typename segmented_iterator_traits<local_iterator>::is_segmented_iterator is_local_seg_t;
-
-   typename traits::segment_iterator scur  = traits::segment(first);
-   typename traits::segment_iterator slast = traits::segment(last);
-   local_iterator lcur = traits::local(first);
-
-   if(scur == slast) {
-      result = copy_n_scan(lcur, traits::local(last), count, result, is_local_seg_t());
-   }
-   else {
-      result = copy_n_scan(lcur, traits::end(scur), count, result, is_local_seg_t());
-      for(++scur; scur != slast && count > 0; ++scur)
-         result = copy_n_scan(traits::begin(scur), traits::end(scur), count, result, is_local_seg_t());
-      if(count > 0)
-         result = copy_n_scan(traits::begin(scur), traits::local(last), count, result, is_local_seg_t());
-   }
-   return result;
-}
-*/
 } // namespace container
 } // namespace boost
 
