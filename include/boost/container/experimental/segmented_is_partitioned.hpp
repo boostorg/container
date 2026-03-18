97a90eed77adaa8f37b4bccc2204b86c509864c0
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-18 16:32:28 +0100

    Re-implement as  segmented_find_if_not + segmented_none_of

--- include/boost/container/experimental/segmented_is_partitioned.hpp
+++ include/boost/container/experimental/segmented_is_partitioned.hpp
@@ -21,94 +21,23 @@
 #include <boost/container/detail/config_begin.hpp>
 #include <boost/container/detail/workaround.hpp>
 #include <boost/container/experimental/segmented_iterator_traits.hpp>
+#include <boost/container/experimental/segmented_find_if_not.hpp>
+#include <boost/container/experimental/segmented_none_of.hpp>
 
 namespace boost {
 namespace container {
-
-template <class InpIter, class Sent, class Pred>
-bool segmented_is_partitioned(InpIter first, Sent last, Pred pred);
-
-namespace detail_algo {
-
-template <class InpIter, class Sent, class Pred>
-bool is_partitioned_with_state(InpIter first, Sent last, Pred pred, bool& in_true_part)
-{
-   if(in_true_part) {
-      for (; first != last; ++first) {
-         if (!pred(*first)) {
-            in_true_part = false;
-            ++first;
-            break;
-         }
-      }
-   }
-
-   for(; first != last; ++first) {
-      if(pred(*first))
-         return false;
-   }
-   return true;
-}
-
-
-// Non-recursive: must carry cross-segment state (in_true_part) to detect
-// the transition from true to false partition across segment boundaries.
-template <class SegIter, class Pred>
-bool segmented_is_partitioned_dispatch
-   (SegIter first, SegIter last, Pred pred, segmented_iterator_tag)
-{
-   typedef segmented_iterator_traits<SegIter> traits;
-   typename traits::segment_iterator sfirst = traits::segment(first);
-   typename traits::segment_iterator slast  = traits::segment(last);
-   bool in_true_part = true;
-   if(sfirst == slast) {
-      return (is_partitioned_with_state)(traits::local(first), traits::local(last), pred, in_true_part);
-   }
-   else {
-      if(!(is_partitioned_with_state)(traits::local(first), traits::end(sfirst), pred, in_true_part))
-         return false;
-
-      for(++sfirst; sfirst != slast; ++sfirst)
-         if(!(is_partitioned_with_state)(traits::begin(sfirst), traits::end(sfirst), pred, in_true_part))
-            return false;
-
-      return (is_partitioned_with_state)(traits::begin(sfirst), traits::local(last), pred, in_true_part);
-   }
-}
-
-template <class InpIter, class Sent, class Pred, class Tag>
-BOOST_CONTAINER_FORCEINLINE typename algo_enable_if_c<
-   !Tag::value || is_sentinel<Sent, InpIter>::value, bool>::type
-segmented_is_partitioned_dispatch(InpIter first, Sent last, Pred pred, Tag)
-{
-   bool in_true_part = true;
-   return (is_partitioned_with_state)(first, last, pred, in_true_part);
-}
-
-} // namespace detail_algo
 
 //! Returns \c true if all elements satisfying \c pred appear before
 //! all elements that do not, i.e. the range [first, last) is
 //! partitioned with respect to \c pred.
 //! Exploits segmentation when available.
 template <class InpIter, class Sent, class Pred>
-BOOST_CONTAINER_FORCEINLINE bool segmented_is_partitioned(InpIter first, Sent last, Pred pred)
+BOOST_CONTAINER_FORCEINLINE
+bool segmented_is_partitioned(InpIter first, Sent last, Pred pred)
 {
-   typedef segmented_iterator_traits<InpIter> traits;
-   return detail_algo::segmented_is_partitioned_dispatch
-      (first, last, pred, typename traits::is_segmented_iterator());
+    first = segmented_find_if_not(first, last, pred);
+    return segmented_none_of(first, last, pred);
 }
-
-/*
-Maybe implement as:
-
-template <class InputIt, class UnaryPredicate>
-bool is_partitioned(InputIt first, InputIt last, UnaryPredicate p) {
-    first = std::find_if_not(first, last, p);
-    return std::none_of(first, last, p);
-}
-
-*/
 
 } // namespace container
 } // namespace boost
