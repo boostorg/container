c5a54b13a926b3f4b402f0e6ebdfcb4afc7066f4
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-22 17:30:06 +0100

    Simplify partition_copy implementation.

--- include/boost/container/experimental/segmented_partition_copy.hpp
+++ include/boost/container/experimental/segmented_partition_copy.hpp
@@ -32,42 +32,6 @@ segmented_partition_copy(InIter first, Sent last, OutIter1 out_true, OutIter2 ou
 
 namespace detail_algo {
 
-template <class SegIter, class OutIter1, class OutIter2, class Pred>
-std::pair<OutIter1, OutIter2>
-segmented_partition_copy_dispatch
-   (SegIter first, SegIter last, OutIter1 out_true, OutIter2 out_false, Pred pred, segmented_iterator_tag)
-{
-   typedef segmented_iterator_traits<SegIter> traits;
-   typedef std::pair<OutIter1, OutIter2> pair_t;
-
-   typename traits::segment_iterator sfirst = traits::segment(first);
-   typename traits::segment_iterator slast  = traits::segment(last);
-
-   if(sfirst == slast) {
-      pair_t p = (segmented_partition_copy)(traits::local(first), traits::local(last), out_true, out_false, pred);
-      out_true  = p.first;
-      out_false = p.second;
-   }
-   else {
-      {
-         pair_t p = (segmented_partition_copy)(traits::local(first), traits::end(sfirst), out_true, out_false, pred);
-         out_true  = p.first;
-         out_false = p.second;
-      }
-      for(++sfirst; sfirst != slast; ++sfirst) {
-         pair_t p = (segmented_partition_copy)(traits::begin(sfirst), traits::end(sfirst), out_true, out_false, pred);
-         out_true  = p.first;
-         out_false = p.second;
-      }
-      {
-         pair_t p = (segmented_partition_copy)(traits::begin(sfirst), traits::local(last), out_true, out_false, pred);
-         out_true  = p.first;
-         out_false = p.second;
-      }
-   }
-   return pair_t(out_true, out_false);
-}
-
 template <class InIter, class Sent, class OutIter1, class OutIter2, class Pred, class Tag>
 typename algo_enable_if_c<
    !Tag::value || is_sentinel<Sent, InIter>::value, std::pair<OutIter1, OutIter2> >::type
@@ -85,6 +49,34 @@ segmented_partition_copy_dispatch
       }
    }
    return std::pair<OutIter1, OutIter2>(out_true, out_false);
+}
+
+template <class SegIter, class OutIter1, class OutIter2, class Pred>
+std::pair<OutIter1, OutIter2>
+segmented_partition_copy_dispatch
+   (SegIter first, SegIter last, OutIter1 out_true, OutIter2 out_false, Pred pred, segmented_iterator_tag)
+{
+   typedef segmented_iterator_traits<SegIter>  traits;
+   typedef typename traits::local_iterator   local_iterator;
+   typedef typename traits::segment_iterator segment_iterator;
+   typedef typename segmented_iterator_traits<local_iterator>::is_segmented_iterator is_local_seg_t;
+   typedef std::pair<OutIter1, OutIter2> pair_t;
+
+   segment_iterator sfirst = traits::segment(first);
+   segment_iterator slast  = traits::segment(last);
+
+   if(sfirst == slast) {
+      return (segmented_partition_copy_dispatch)(traits::local(first), traits::local(last), out_true, out_false, pred, is_local_seg_t());
+   }
+   else {
+      pair_t p = (segmented_partition_copy_dispatch)(traits::local(first), traits::end(sfirst), out_true, out_false, pred, is_local_seg_t());
+
+      for(++sfirst; sfirst != slast; ++sfirst) {
+         p = (segmented_partition_copy_dispatch)(traits::begin(sfirst), traits::end(sfirst), p.first, p.second, pred, is_local_seg_t());
+      }
+
+      return (segmented_partition_copy_dispatch)(traits::begin(sfirst), traits::local(last), p.first, p.second, pred, is_local_seg_t());
+   }
 }
 
 } // namespace detail_algo
