d26c4c4ae1eaf00aa40f1400684badd38c7a03d0
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-22 17:32:41 +0100

    Simplify implementation to ease work to the compiler and rely less on inlining

--- include/boost/container/experimental/segmented_remove_copy_if.hpp
+++ include/boost/container/experimental/segmented_remove_copy_if.hpp
@@ -30,27 +30,6 @@ OutIter segmented_remove_copy_if(InIter first, Sent last, OutIter result, Pred p
 
 namespace detail_algo {
 
-template <class SegIter, class OutIter, class Pred>
-OutIter segmented_remove_copy_if_dispatch
-   (SegIter first, SegIter last, OutIter result, Pred pred, segmented_iterator_tag)
-{
-   typedef segmented_iterator_traits<SegIter> traits;
-   typename traits::segment_iterator sfirst = traits::segment(first);
-   typename traits::segment_iterator slast  = traits::segment(last);
-
-   if(sfirst == slast) {
-      return boost::container::segmented_remove_copy_if(traits::local(first), traits::local(last), result, pred);
-   }
-   else {
-      result = boost::container::segmented_remove_copy_if(traits::local(first), traits::end(sfirst), result, pred);
-
-      for(++sfirst; sfirst != slast; ++sfirst)
-         result = boost::container::segmented_remove_copy_if(traits::begin(sfirst), traits::end(sfirst), result, pred);
-
-      return boost::container::segmented_remove_copy_if(traits::begin(sfirst), traits::local(last), result, pred);
-   }
-}
-
 template <class InIter, class Sent, class OutIter, class Pred, class Tag>
 typename algo_enable_if_c<
    !Tag::value || is_sentinel<Sent, InIter>::value, OutIter>::type
@@ -66,13 +45,39 @@ segmented_remove_copy_if_dispatch
    return result;
 }
 
+template <class SegIter, class OutIter, class Pred>
+OutIter segmented_remove_copy_if_dispatch
+   (SegIter first, SegIter last, OutIter result, Pred pred, segmented_iterator_tag)
+{
+   typedef segmented_iterator_traits<SegIter>  traits;
+   typedef typename traits::local_iterator   local_iterator;
+   typedef typename traits::segment_iterator segment_iterator;
+   typedef typename segmented_iterator_traits<local_iterator>::is_segmented_iterator is_local_seg_t;
+
+   segment_iterator sfirst = traits::segment(first);
+   segment_iterator slast  = traits::segment(last);
+
+   if(sfirst == slast) {
+      return (segmented_remove_copy_if_dispatch)(traits::local(first), traits::local(last), result, pred, is_local_seg_t());
+   }
+   else {
+      result = (segmented_remove_copy_if_dispatch)(traits::local(first), traits::end(sfirst), result, pred, is_local_seg_t());
+
+      for(++sfirst; sfirst != slast; ++sfirst)
+         result = (segmented_remove_copy_if_dispatch)(traits::begin(sfirst), traits::end(sfirst), result, pred, is_local_seg_t());
+
+      return (segmented_remove_copy_if_dispatch)(traits::begin(sfirst), traits::local(last), result, pred, is_local_seg_t());
+   }
+}
+
 } // namespace detail_algo
 
 //! Copies elements from [first, last) to the range beginning at \c result,
 //! skipping elements satisfying \c pred. Returns the output iterator past
 //! the last element written.
 template <class InIter, class Sent, class OutIter, class Pred>
-BOOST_CONTAINER_FORCEINLINE OutIter segmented_remove_copy_if(InIter first, Sent last, OutIter result, Pred pred)
+BOOST_CONTAINER_FORCEINLINE
+OutIter segmented_remove_copy_if(InIter first, Sent last, OutIter result, Pred pred)
 {
    typedef segmented_iterator_traits<InIter> traits;
    return detail_algo::segmented_remove_copy_if_dispatch
