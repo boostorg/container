9113249359bfeda5fbc727fffdd2e4a0a8bbfc6a
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-23 12:03:34 +0100

    Add additional tests for special segment conditions in code

--- experimental/segmented_reverse_test.cpp
+++ experimental/segmented_reverse_test.cpp
@@ -98,6 +98,66 @@ void test_reverse_seg2()
       BOOST_TEST_EQ(*it, expected[i]);
 }
 
+void test_special_segment_conditions()
+{
+   {  //Test consuming both segments at the same time with no middle segment
+      test_detail::seg2_vector<int> sv2;
+      int a1[] = { 1, 2, 3 };
+      int a2[] = { 4, 5, 6 };
+      int a3[] = { 7, 8, 9 };
+      int a4[] = { 10, 11, 12 };
+      sv2.add_flat_segment_range(a1, a1 + 3);
+      sv2.add_flat_segment_range(a2, a2 + 3);
+      sv2.add_flat_segment_range(a3, a3 + 3);
+      sv2.add_flat_segment_range(a4, a4 + 3);
+
+      segmented_reverse(sv2.begin(), sv2.end());
+
+      int expected[] = { 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+      test_detail::seg2_vector<int>::iterator it = sv2.begin();
+      for (int i = 0; i < 9; ++i, ++it)
+         BOOST_TEST_EQ(*it, expected[i]);
+   }
+
+   {  //Test consuming both segments with a single in the middle
+      test_detail::seg2_vector<int> sv2;
+      int a1[] = { 1, 2, 3 };
+      int a2[] = { 4, 5, 6 };
+      int a3[] = { 7, 8, 9 };
+      sv2.add_flat_segment_range(a1, a1 + 3);
+      sv2.add_flat_segment_range(a2, a2 + 3);
+      sv2.add_flat_segment_range(a3, a3 + 3);
+
+      segmented_reverse(sv2.begin(), sv2.end());
+
+      int expected[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+      test_detail::seg2_vector<int>::iterator it = sv2.begin();
+      for (int i = 0; i < 9; ++i, ++it)
+         BOOST_TEST_EQ(*it, expected[i]);
+   }
+   {  //Asymetric comsumption of segments
+      test_detail::seg2_vector<int> sv2;
+      int a1[] = { 1, 2, 3, 4 };
+      int a2[] = { 5, 6, 7, 8 };
+      int a3[] = { 9, 10, 11 };
+      int a4[] = { 12, 13 };
+      int a5[] = { 14, 15,16 };
+      sv2.add_flat_segment_range(a1, a1 + 4);
+      sv2.add_flat_segment_range(a2, a2 + 4);
+      sv2.add_flat_segment_range(a3, a3 + 3);
+      sv2.add_flat_segment_range(a4, a4 + 2);
+      sv2.add_flat_segment_range(a5, a5 + 3);
+
+      segmented_reverse(sv2.begin(), sv2.end());
+
+      int expected[] = { 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+      test_detail::seg2_vector<int>::iterator it = sv2.begin();
+      for (int i = 0; i < 9; ++i, ++it)
+         BOOST_TEST_EQ(*it, expected[i]);
+   }
+}
+
+
 int main()
 {
    test_reverse_segmented();
@@ -106,5 +166,6 @@ int main()
    test_reverse_even_count();
    test_reverse_non_segmented();
    test_reverse_seg2();
+   test_special_segment_conditions();
    return boost::report_errors();
 }
