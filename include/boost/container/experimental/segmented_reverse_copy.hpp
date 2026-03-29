63a4df535e67e56c1ea51d38966f9a125284121a
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-29 18:57:18 +0200

    Unrolled segmented_reverse_copy

--- include/boost/container/experimental/segmented_reverse_copy.hpp
+++ include/boost/container/experimental/segmented_reverse_copy.hpp
@@ -21,6 +21,7 @@
 #include <boost/container/detail/config_begin.hpp>
 #include <boost/container/detail/workaround.hpp>
 #include <boost/container/experimental/segmented_iterator_traits.hpp>
+#include <boost/container/detail/iterator.hpp>
 
 namespace boost {
 namespace container {
@@ -32,7 +33,7 @@ namespace detail_algo {
 
 template <class BidirIter, class OutIter>
 OutIter segmented_reverse_copy_dispatch
-   (BidirIter first, BidirIter last, OutIter result, non_segmented_iterator_tag)
+   (BidirIter first, BidirIter last, OutIter result, non_segmented_iterator_tag, const std::bidirectional_iterator_tag &)
 {
    while(first != last) {
       --last;
@@ -42,9 +43,44 @@ OutIter segmented_reverse_copy_dispatch
    return result;
 }
 
-template <class SegIter, class OutIter>
+#if defined(BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING)
+
+template <class RAIter, class OutIter>
 OutIter segmented_reverse_copy_dispatch
-   (SegIter first, SegIter last, OutIter result, segmented_iterator_tag)
+   (RAIter first, RAIter last, OutIter result, non_segmented_iterator_tag, const std::random_access_iterator_tag &)
+{
+   typedef typename iterator_traits<RAIter>::difference_type difference_type;
+   difference_type n = last - first;
+
+   while(n >= difference_type(4)) {
+      --last; *result = *last; ++result;
+      --last; *result = *last; ++result;
+      --last; *result = *last; ++result;
+      --last; *result = *last; ++result;
+      n -= 4;
+   }
+
+   switch (n % 4) {
+      case 3:
+         --last; *result = *last; ++result;
+      BOOST_FALLTHROUGH;
+      case 2:
+         --last; *result = *last; ++result;
+      BOOST_FALLTHROUGH;
+      case 1:
+         --last; *result = *last; ++result;
+      BOOST_FALLTHROUGH;
+      default:
+         break;
+   }
+   return result;
+}
+
+#endif   //BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING
+
+template <class SegIter, class OutIter, class Cat>
+OutIter segmented_reverse_copy_dispatch
+   (SegIter first, SegIter last, OutIter result, segmented_iterator_tag, Cat)
 {
    typedef segmented_iterator_traits<SegIter>   traits;
    typedef typename traits::local_iterator    local_iterator;
@@ -56,15 +92,15 @@ OutIter segmented_reverse_copy_dispatch
    segment_iterator       slast  = traits::segment(last);
 
    if(sfirst == slast) {
-      return (segmented_reverse_copy_dispatch)(traits::local(first), traits::local(last), result, is_local_seg_t());
+      return (segmented_reverse_copy_dispatch)(traits::local(first), traits::local(last), result, is_local_seg_t(), Cat());
    }
    else {
-      result = (segmented_reverse_copy_dispatch)(traits::begin(slast), traits::local(last), result, is_local_seg_t());
+      result = (segmented_reverse_copy_dispatch)(traits::begin(slast), traits::local(last), result, is_local_seg_t(), Cat());
 
       for (--slast; slast != sfirst; --slast)
-         result = (segmented_reverse_copy_dispatch)(traits::begin(slast), traits::end(slast), result, is_local_seg_t());
+         result = (segmented_reverse_copy_dispatch)(traits::begin(slast), traits::end(slast), result, is_local_seg_t(), Cat());
 
-      return (segmented_reverse_copy_dispatch)(traits::local(first), traits::end(sfirst), result, is_local_seg_t());
+      return (segmented_reverse_copy_dispatch)(traits::local(first), traits::end(sfirst), result, is_local_seg_t(), Cat());
    }
 }
 
@@ -80,7 +116,7 @@ BOOST_CONTAINER_FORCEINLINE OutIter segmented_reverse_copy(BidirIter first, Bidi
 {
    typedef segmented_iterator_traits<BidirIter> traits;
    return detail_algo::segmented_reverse_copy_dispatch
-      (first, last, result, typename traits::is_segmented_iterator());
+      (first, last, result, typename traits::is_segmented_iterator(), typename iterator_traits<BidirIter>::iterator_category());
 }
 
 } // namespace container
