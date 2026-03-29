3d36bd57b3a95466f79a74add5eca058b1599bd9
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-29 17:26:30 +0200

    Unroll segmented_is_sorted_until

--- include/boost/container/experimental/segmented_is_sorted_until.hpp
+++ include/boost/container/experimental/segmented_is_sorted_until.hpp
@@ -21,6 +21,7 @@
 #include <boost/container/detail/config_begin.hpp>
 #include <boost/container/detail/workaround.hpp>
 #include <boost/container/experimental/segmented_iterator_traits.hpp>
+#include <boost/container/detail/iterator.hpp>
 
 namespace boost {
 namespace container {
@@ -35,9 +36,9 @@ struct default_less
 
 // Non-recursive: must carry cross-segment state (prev_local) to compare
 // the last element of one segment with the first of the next.
-template <class SegIter, class Comp>
+template <class SegIter, class Comp, class Cat>
 SegIter segmented_is_sorted_until_dispatch
-   (SegIter first, SegIter last, Comp comp, segmented_iterator_tag)
+   (SegIter first, SegIter last, Comp comp, segmented_iterator_tag, Cat)
 {
    typedef segmented_iterator_traits<SegIter>   traits;
    typedef typename traits::segment_iterator    segment_iterator;
@@ -116,10 +117,69 @@ SegIter segmented_is_sorted_until_dispatch
    return last;
 }
 
-template <class FwdIt, class Sent, class Comp, class Tag>
+#if defined(BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING)
+
+template <class RAIter, class Comp>
+RAIter segmented_is_sorted_until_dispatch
+   (RAIter first, RAIter last, Comp comp, const non_segmented_iterator_tag &, const std::random_access_iterator_tag &)
+{
+   typedef typename iterator_traits<RAIter>::difference_type difference_type;
+
+   difference_type n = last - first;
+   if(n <= difference_type(1))
+      return last;
+
+   RAIter prev = first;
+   ++first;
+   --n;
+
+   while(n >= difference_type(4)) {
+      if(comp(*first, *prev))
+         goto final_result;
+      prev = first; ++first;
+      if(comp(*first, *prev))
+         goto final_result;
+      prev = first; ++first;
+      if(comp(*first, *prev))
+         goto final_result;
+      prev = first; ++first;
+      if(comp(*first, *prev))
+         goto final_result;
+      prev = first; ++first;
+      n -= 4;
+   }
+
+   switch (n % 4) {
+      case 3:
+         if(comp(*first, *prev))
+            break;
+         prev = first; ++first;
+      BOOST_FALLTHROUGH;
+      case 2:
+         if(comp(*first, *prev))
+            break;
+         prev = first; ++first;
+      BOOST_FALLTHROUGH;
+      case 1:
+         if(comp(*first, *prev))
+            break;
+         //No need to save "prev" since we're done after this.
+         ++first;
+      BOOST_FALLTHROUGH;
+      default:
+         break;
+   }
+
+   final_result:
+   return first;
+}
+
+#endif   //BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING
+
+template <class FwdIt, class Sent, class Comp, class Tag, class Cat>
 typename algo_enable_if_c<
    !Tag::value || is_sentinel<Sent, FwdIt>::value, FwdIt>::type
-segmented_is_sorted_until_dispatch(FwdIt first, Sent last, Comp comp, Tag)
+segmented_is_sorted_until_dispatch(FwdIt first, Sent last, Comp comp, Tag, Cat)
 {
    if(first == last)
       return last;
@@ -146,7 +206,7 @@ FwdIt segmented_is_sorted_until(FwdIt first, Sent last, Comp comp)
 {
    typedef segmented_iterator_traits<FwdIt> traits;
    return detail_algo::segmented_is_sorted_until_dispatch
-      (first, last, comp,typename traits::is_segmented_iterator());
+      (first, last, comp, typename traits::is_segmented_iterator(), typename iterator_traits<FwdIt>::iterator_category());
 }
 
 //! Returns an iterator to the first element that is less than its predecessor
@@ -158,7 +218,7 @@ FwdIt segmented_is_sorted_until(FwdIt first, Sent last)
 {
    typedef segmented_iterator_traits<FwdIt> traits;
    return detail_algo::segmented_is_sorted_until_dispatch
-      (first, last, detail_algo::default_less(), typename traits::is_segmented_iterator());
+      (first, last, detail_algo::default_less(), typename traits::is_segmented_iterator(), typename iterator_traits<FwdIt>::iterator_category());
 }
 
 } // namespace container
