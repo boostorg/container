6d935f243e884ff3ecf143a648b154c33b4e0cbd
Ion Gaztañaga <igaztanaga@gmail.com>
2026-03-29 15:41:18 +0200

    Unroll segmented_generate

--- include/boost/container/experimental/segmented_generate.hpp
+++ include/boost/container/experimental/segmented_generate.hpp
@@ -21,6 +21,7 @@
 #include <boost/container/detail/config_begin.hpp>
 #include <boost/container/detail/workaround.hpp>
 #include <boost/container/experimental/segmented_iterator_traits.hpp>
+#include <boost/container/detail/iterator.hpp>
 
 namespace boost {
 namespace container {
@@ -30,18 +31,52 @@ void segmented_generate(FwdIt first, Sent last, Generator gen);
 
 namespace detail_algo {
 
-template <class FwdIt, class Sent, class Generator, class Tag>
+#if defined(BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING)
+
+template <class RAIter, class Generator>
+void segmented_generate_dispatch
+   (RAIter first, RAIter last, Generator &gen, const non_segmented_iterator_tag &, const std::random_access_iterator_tag &)
+{
+   typedef typename iterator_traits<RAIter>::difference_type difference_type;
+
+   difference_type n = last - first;
+   while(n >= difference_type(4)) {
+      *first = gen(); ++first;
+      *first = gen(); ++first;
+      *first = gen(); ++first;
+      *first = gen(); ++first;
+      n -= 4;
+   }
+
+   switch (n % 4) {
+      case 3:
+         *first = gen(); ++first;
+      BOOST_FALLTHROUGH;
+      case 2:
+         *first = gen(); ++first;
+      BOOST_FALLTHROUGH;
+      case 1:
+         *first = gen(); ++first;
+      BOOST_FALLTHROUGH;
+      default:
+         break;
+   }
+}
+
+#endif   //BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING
+
+template <class FwdIt, class Sent, class Generator, class Tag, class Cat>
  BOOST_CONTAINER_FORCEINLINE typename algo_enable_if_c<
    !Tag::value || is_sentinel<Sent, FwdIt>::value>::type
-segmented_generate_dispatch(FwdIt first, Sent last, Generator &gen, Tag)
+segmented_generate_dispatch(FwdIt first, Sent last, Generator &gen, Tag, Cat)
 {
    for(; first != last; ++first)
       *first = gen();
 }
 
-template <class SegIter, class Generator>
+template <class SegIter, class Generator, class Cat>
 void segmented_generate_dispatch
-   (SegIter first, SegIter last, Generator &gen, segmented_iterator_tag)
+   (SegIter first, SegIter last, Generator &gen, segmented_iterator_tag, Cat)
 {
    typedef segmented_iterator_traits<SegIter>   traits;
    typedef typename traits::local_iterator      local_iterator;
@@ -52,15 +87,15 @@ void segmented_generate_dispatch
    segment_iterator slast  = traits::segment(last);
 
    if(sfirst == slast) {
-      (segmented_generate_dispatch)(traits::local(first), traits::local(last), gen, is_local_seg_t());
+      (segmented_generate_dispatch)(traits::local(first), traits::local(last), gen, is_local_seg_t(), Cat());
    }
    else {
-      (segmented_generate_dispatch)(traits::local(first), traits::end(sfirst), gen, is_local_seg_t());
+      (segmented_generate_dispatch)(traits::local(first), traits::end(sfirst), gen, is_local_seg_t(), Cat());
 
       for(++sfirst; sfirst != slast; ++sfirst)
-         (segmented_generate_dispatch)(traits::begin(sfirst), traits::end(sfirst), gen, is_local_seg_t());
+         (segmented_generate_dispatch)(traits::begin(sfirst), traits::end(sfirst), gen, is_local_seg_t(), Cat());
 
-      (segmented_generate_dispatch)(traits::begin(sfirst), traits::local(last), gen, is_local_seg_t());
+      (segmented_generate_dispatch)(traits::begin(sfirst), traits::local(last), gen, is_local_seg_t(), Cat());
    }
 }
 
@@ -74,7 +109,8 @@ BOOST_CONTAINER_FORCEINLINE
 void segmented_generate(FwdIt first, Sent last, Generator gen)
 {
    typedef segmented_iterator_traits<FwdIt> traits;
-   detail_algo::segmented_generate_dispatch(first, last, gen, typename traits::is_segmented_iterator());
+   detail_algo::segmented_generate_dispatch
+      (first, last, gen, typename traits::is_segmented_iterator(), typename iterator_traits<FwdIt>::iterator_category());
 }
 
 } // namespace container
