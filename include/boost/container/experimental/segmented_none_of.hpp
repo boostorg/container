eadc1fe6e9878682970e03df167ad3526a5b31d7
Ion Gaztañaga <igaztanaga@gmail.com>
2026-04-03 15:29:27 +0200

    Implemented directly segmented_none_of for better performance

--- include/boost/container/experimental/segmented_none_of.hpp
+++ include/boost/container/experimental/segmented_none_of.hpp
@@ -20,10 +20,111 @@
 
 #include <boost/container/detail/config_begin.hpp>
 #include <boost/container/detail/workaround.hpp>
-#include <boost/container/experimental/segmented_find_if.hpp>
+#include <boost/container/experimental/segmented_iterator_traits.hpp>
+#include <boost/container/detail/iterators.hpp>
 
 namespace boost {
 namespace container {
+
+template <class InpIter, class Sent, class Pred>
+bool segmented_none_of(InpIter first, Sent last, Pred pred);
+
+namespace detail_algo {
+
+#if defined(BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING)
+
+template <class RAIter, class Pred>
+bool segmented_none_of_dispatch
+   (RAIter first, RAIter last, Pred pred, const non_segmented_iterator_tag &, const std::random_access_iterator_tag &)
+{
+   typedef typename iterator_traits<RAIter>::difference_type difference_type;
+
+   difference_type n = last - first;
+   while(n >= difference_type(4)) {
+      if(pred(*first))
+         return false;
+      ++first;
+      if(pred(*first))
+         return false;
+      ++first;
+      if(pred(*first))
+         return false;
+      ++first;
+      if(pred(*first))
+         return false;
+      ++first;
+      n -= 4;
+   }
+
+   switch(n) {
+      case 3:
+         if(pred(*first))
+            return false;
+         ++first;
+         BOOST_FALLTHROUGH;
+      case 2:
+         if(pred(*first))
+            return false;
+         ++first;
+         BOOST_FALLTHROUGH;
+      case 1:
+         if(pred(*first))
+            return false;
+         ++first;
+         BOOST_FALLTHROUGH;
+      default:
+         break;
+   }
+   return true;
+}
+
+#endif   //BOOST_CONTAINER_SEGMENTED_LOOP_UNROLLING
+
+template <class InpIter, class Sent, class Pred, class Tag, class Cat>
+typename algo_enable_if_c<
+   !Tag::value || is_sentinel<Sent, InpIter>::value, bool>::type
+segmented_none_of_dispatch(InpIter first, Sent last, Pred pred, Tag, Cat)
+{
+   for(; first != last; ++first)
+      if(pred(*first))
+         return false;
+   return true;
+}
+
+template <class SegIter, class Pred, class Cat>
+bool segmented_none_of_dispatch
+   (SegIter first, SegIter last, Pred pred, segmented_iterator_tag, Cat)
+{
+   typedef segmented_iterator_traits<SegIter> traits;
+   typedef typename traits::local_iterator    local_iterator;
+   typedef typename traits::segment_iterator  segment_iterator;
+   typedef typename segmented_iterator_traits<local_iterator>::is_segmented_iterator is_local_seg_t;
+   typedef typename iterator_traits<local_iterator>::iterator_category local_cat_t;
+
+   segment_iterator       sfirst = traits::segment(first);
+   const segment_iterator slast  = traits::segment(last);
+
+   if(sfirst == slast) {
+      if(!(segmented_none_of_dispatch)(traits::local(first), traits::local(last), pred, is_local_seg_t(), local_cat_t()))
+         return false;
+   }
+   else {
+      //First segment
+      if(!(segmented_none_of_dispatch)(traits::local(first), traits::end(sfirst), pred, is_local_seg_t(), local_cat_t()))
+         return false;
+      //Middle segments
+      for (++sfirst; sfirst != slast; ++sfirst) {
+         if(!(segmented_none_of_dispatch)(traits::begin(sfirst), traits::end(sfirst), pred, is_local_seg_t(), local_cat_t()))
+            return false;
+      }
+      //Last segment
+      if(!(segmented_none_of_dispatch)(traits::begin(slast), traits::local(last), pred, is_local_seg_t(), local_cat_t()))
+         return false;
+   }
+   return true;
+}
+
+} // namespace detail_algo
 
 //! Returns \c true if \c pred returns false for all elements
 //! in [first, last), or if the range is empty.
@@ -31,7 +132,9 @@ template <class InpIter, class Sent, class Pred>
 BOOST_CONTAINER_FORCEINLINE
 bool segmented_none_of(InpIter first, Sent last, Pred pred)
 {
-   return (segmented_find_if)(first, last, pred) == last;
+   typedef segmented_iterator_traits<InpIter> traits;
+   return detail_algo::segmented_none_of_dispatch
+      (first, last, pred, typename traits::is_segmented_iterator(), typename iterator_traits<InpIter>::iterator_category());
 }
 
 } // namespace container
