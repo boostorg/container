4e3dac1c5d6dc7c22443c866273882f4f7a3f928
Ion Gaztañaga <igaztanaga@gmail.com>
2026-06-15 23:22:08 +0200

    Update copyright.

--- test/hub_sort_test.cpp
+++ test/hub_sort_test.cpp
@@ -1,4 +1,5 @@
 /* Copyright 2026 Joaquin M Lopez Munoz.
+ * Copyright 2026 Ion Gaztanaga.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
