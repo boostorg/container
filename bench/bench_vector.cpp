4edcf2ae9ffb16b7fbd29397b068ab61e070d358
Ion Gaztañaga <igaztanaga@gmail.com>
2026-07-16 00:22:22 +0200

    Use default allocator (void) for benchmarks.

--- bench/bench_vector.cpp
+++ bench/bench_vector.cpp
@@ -27,9 +27,9 @@ void run_containers(runner<IntType, Operation>& r)
 {
    //First registered container is the baseline (denominator).
    r.template add< std::vector<IntType, std::allocator<IntType> > >("std::vector");
-   r.template add< bc::vector<IntType, std::allocator<IntType> >  >("vector(1.6x)");
+   r.template add< bc::vector<IntType, void >  >("vector(1.6x)");
    typedef typename bc::vector_options < bc::growth_factor<bc::growth_factor_100> >::type growth_100_t;
-   r.template add< bc::vector<IntType, std::allocator<IntType>, growth_100_t> >("vector(2x)");
+   r.template add< bc::vector<IntType, void, growth_100_t> >("vector(2x)");
 }
 
 int main()
