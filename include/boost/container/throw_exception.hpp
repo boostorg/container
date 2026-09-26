01e7025237e7ae5de4e615b9a14ad557cc04ad8d
Ion Gaztañaga <igaztanaga@gmail.com>
2026-09-26 11:33:38 +0200

    Avoid -Wstring-conversion in BOOST_NO_EXCEPTIONS throw callbacks

--- include/boost/container/throw_exception.hpp
+++ include/boost/container/throw_exception.hpp
@@ -149,35 +149,35 @@ namespace container {
 
    BOOST_NORETURN inline void throw_bad_alloc()
    {
-      BOOST_ASSERT(!"boost::container bad_alloc thrown");
+      BOOST_ASSERT(false && "boost::container bad_alloc thrown");
       std::abort();
    }
 
    BOOST_NORETURN inline void throw_out_of_range(const char* str)
    {
       boost::container::ignore(str);
-      BOOST_ASSERT_MSG(!"boost::container out_of_range thrown", str);
+      BOOST_ASSERT_MSG(false && "boost::container out_of_range thrown", str);
       std::abort();
    }
 
    BOOST_NORETURN inline void throw_length_error(const char* str)
    {
       boost::container::ignore(str);
-      BOOST_ASSERT_MSG(!"boost::container length_error thrown", str);
+      BOOST_ASSERT_MSG(false && "boost::container length_error thrown", str);
       std::abort();
    }
 
    BOOST_NORETURN inline void throw_logic_error(const char* str)
    {
       boost::container::ignore(str);
-      BOOST_ASSERT_MSG(!"boost::container logic_error thrown", str);
+      BOOST_ASSERT_MSG(false && "boost::container logic_error thrown", str);
       std::abort();
    }
 
    BOOST_NORETURN inline void throw_runtime_error(const char* str)
    {
       boost::container::ignore(str);
-      BOOST_ASSERT_MSG(!"boost::container runtime_error thrown", str);
+      BOOST_ASSERT_MSG(false && "boost::container runtime_error thrown", str);
       std::abort();
    }
 
@@ -192,7 +192,7 @@ namespace container {
    //!   <code>std::bad_alloc(str)</code> is thrown.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS is defined and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS
-   //!   is NOT defined <code>BOOST_ASSERT(!"boost::container bad_alloc thrown")</code> is called
+   //!   is NOT defined <code>BOOST_ASSERT(false && "boost::container bad_alloc thrown")</code> is called
    //!   and <code>std::abort()</code> if the former returns.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS are defined
@@ -212,7 +212,7 @@ namespace container {
    //!   <code>std::out_of_range(str)</code> is thrown.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS is defined and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS
-   //!   is NOT defined <code>BOOST_ASSERT_MSG(!"boost::container out_of_range thrown", str)</code> is called
+   //!   is NOT defined <code>BOOST_ASSERT_MSG(false && "boost::container out_of_range thrown", str)</code> is called
    //!   and <code>std::abort()</code> if the former returns.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS are defined
@@ -233,7 +233,7 @@ namespace container {
    //!   <code>std::length_error(str)</code> is thrown.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS is defined and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS
-   //!   is NOT defined <code>BOOST_ASSERT_MSG(!"boost::container length_error thrown", str)</code> is called
+   //!   is NOT defined <code>BOOST_ASSERT_MSG(false && "boost::container length_error thrown", str)</code> is called
    //!   and <code>std::abort()</code> if the former returns.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS are defined
@@ -255,7 +255,7 @@ namespace container {
    //!   <code>std::logic_error(str)</code> is thrown.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS is defined and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS
-   //!   is NOT defined <code>BOOST_ASSERT_MSG(!"boost::container logic_error thrown", str)</code> is called
+   //!   is NOT defined <code>BOOST_ASSERT_MSG(false && "boost::container logic_error thrown", str)</code> is called
    //!   and <code>std::abort()</code> if the former returns.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS are defined
@@ -275,7 +275,7 @@ namespace container {
    //!   <code>std::runtime_error(str)</code> is thrown.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS is defined and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS
-   //!   is NOT defined <code>BOOST_ASSERT_MSG(!"boost::container runtime_error thrown", str)</code> is called
+   //!   is NOT defined <code>BOOST_ASSERT_MSG(false && "boost::container runtime_error thrown", str)</code> is called
    //!   and <code>std::abort()</code> if the former returns.</li>
    //!
    //! <li>If BOOST_NO_EXCEPTIONS and BOOST_CONTAINER_USER_DEFINED_THROW_CALLBACKS are defined
